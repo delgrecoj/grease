a15b95efb4bb409a983b982779938b2b81117975
Jesse Del Greco <jesse@jessedelgreco.com>
2020-07-12 15:46:56 -0400

    decide to design around grease impl instead of sugar impl

--- grease.ex
+++ grease.ex
@@ -1,6 +1,15 @@
 defmodule Grease do
-  # duplicate of Sugar.set/1
-  def set([{keyf, keyv}, {valf, valv}]) when is_atom(keyf) and is_atom(valf) do
+  # intention here is to smooth over the same problem Sugar.set/2 does,
+  # except provide cleaner syntax when id atom fields can be assumed to exist.
+  def set(data, keywordlist) when is_list(data) do
+    [fieldpair | [{leadin, fieldpairindex} | rest]] = Enum.reverse(keywordlist)
+    args = [id: fieldpairindex] ++ [fieldpair]
+    setfn = rd(setp(args), leadin, rest)
+    setfn.(data)
+  end
+
+  # duplicate of Sugar.set/1; generate fn for updating particular field with literal or fn
+  defp setp([{keyf, keyv}, {valf, valv}]) when is_atom(keyf) and is_atom(valf) do
     fn data ->
       Enum.map(data, fn map ->
         if map[keyf] == keyv do
@@ -16,38 +25,13 @@ defmodule Grease do
     end
   end
 
-  # duplicate of Sugar.set/2
-  def set(data, [{keyf, keyv}, {valf, valv}])
-      when is_atom(keyf) and is_atom(valf) and is_list(data) do
-    Enum.map(data, fn map ->
-      if map[keyf] == keyv do
-        if is_function(valv) do
-          Map.update!(map, valf, valv)
-        else
-          Map.update!(map, valf, fn _ -> valv end)
-        end
-      else
-        map
-      end
-    end)
-  end
-
-  # intention here is to smooth over the same problem Sugar.set/2 does,
-  # except provide slightly cleaner syntax when id fields can be assumed.
-
-  def set(data, keywordlist) when is_list(data) do
-    [fieldpair | [{leadin, fieldpairindex} | rest]] = Enum.reverse(keywordlist)
-    args = [id: fieldpairindex] ++ [fieldpair]
-    setfn = rd(Grease.set(args), leadin, rest)
-    setfn.(data)
-  end
-
-  # recursively deconstructs the keyword list into nested fncalls
+  # base-case for recursively deconstructing the keyword list
   defp rd(prev, :id, []) when is_function(prev), do: prev
 
+  # recursively deconstructs the keyword list into nested fncalls
   defp rd(prev, leadin, [{new_leadin, v} | rest])
        when is_function(prev) and is_atom(leadin) and is_atom(new_leadin) and is_list(rest) do
-    rd(Grease.set([id: v] ++ [{leadin, prev}]), new_leadin, rest)
+    rd(setp([id: v] ++ [{leadin, prev}]), new_leadin, rest)
   end
 end
 
@@ -72,15 +56,55 @@ defmodule GreaseExamples do
       }
     ]
 
-    # various ways of calling:
     Grease.set(jobs, id: 0, ops: 0, specs: 0, comments: 0, text: "qwerty")
-    Grease.set(jobs, id: 0, ops: 0, specs: 0, comments: 0, text: "qwerty")
-    jobs |> Grease.set(id: 0, ops: 0, specs: 0, comments: 0, text: "qwerty")
 
-    jobs
-    |> IO.inspect(label: "before")
-    |> Grease.set(id: 0, ops: 0, specs: 0, comments: 0, text: "qwerty")
-    |> IO.inspect(label: "after")
+    # various other ways of calling:
+    # Grease.set(jobs, [id: 0, ops: 0, specs: 0, comments: 0, text: "qwerty"])
+    # jobs |> Grease.set(id: 0, ops: 0, specs: 0, comments: 0, text: "qwerty")
+
+    # same as the following:
+    # Enum.map(jobs, fn job ->
+    #   if job.id == 0 do
+    #     %{
+    #       job
+    #       | ops:
+    #           Enum.map(job.ops, fn op ->
+    #             if op.id == 0 do
+    #               %{
+    #                 op
+    #                 | specs:
+    #                     Enum.map(op.specs, fn spec ->
+    #                       if spec.id == 0 do
+    #                         %{
+    #                           spec
+    #                           | comments:
+    #                               Enum.map(spec.comments, fn comment ->
+    #                                 if comment.id == 0 do
+    #                                   %{comment | text: "qwerty"}
+    #                                 else
+    #                                   comment
+    #                                 end
+    #                               end)
+    #                         }
+    #                       else
+    #                         spec
+    #                       end
+    #                     end)
+    #               }
+    #             else
+    #               op
+    #             end
+    #           end)
+    #     }
+    #   else
+    #     job
+    #   end
+    # end)
+
+    # jobs
+    # |> IO.inspect(label: "before")
+    # |> Grease.set(id: 0, ops: 0, specs: 0, comments: 0, text: "qwerty")
+    # |> IO.inspect(label: "after")
   end
 
   def simple_set() do
@@ -103,9 +127,25 @@ defmodule GreaseExamples do
     #   end
     # end)
 
+    # data
+    # |> IO.inspect(label: "before")
+    # |> Grease.set(id: 3, count: 60)
+    # |> IO.inspect(label: "after")
+  end
+
+  def set_using_fn() do
+    data = [
+      %{id: 0, count: 0},
+      %{id: 1, count: 0},
+      %{id: 2, count: 0},
+      %{id: 3, count: 0},
+      %{id: 4, count: 0}
+    ]
+
+    # none of this prevents applying functions instead of literals;
     data
-    |> IO.inspect(label: "before")
-    |> Grease.set(id: 3, count: 60)
-    |> IO.inspect(label: "after")
+    |> Grease.set(id: 3, count: &(&1 + 1))
+    |> Grease.set(id: 3, count: &(&1 + 42))
+    |> Grease.set(id: 3, count: fn c -> c + 3 end)
   end
 end
